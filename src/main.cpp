ad11376c230ccfa6f987f8934f2b38ec366a4cfd
DaSommsi <116731827+DaSommsi@users.noreply.github.com>
2025-05-09 22:44:11 +0200

    Added function to read the data out of the ESP32 buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  ProcessIncomingDataFromSimTools();
 }
 
-// Functionen aus Diplomarbeit
+// Funktionen
+
+// Funktion wird dauerhaft aufgerufen und nimmt die Daten entgegen und verarbeitet sie
+void ProcessIncomingDataFromSimTools(){
+  // Überprüft ob Daten im Buffer sind
+  if (Serial.available() > 0){
+    // Daten aus Buffer holen
+    String incomingData = Serial.readStringUntil('\n');
+    // Daten ausgeben um zum Testen
+    Serial.println(incomingData);
+
+
+  }
+}
+
